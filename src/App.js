242db86dfcb623bb813dc25f7ca6f7eb76a74c68
Everton Buzzi <evertonbuzzi@gmail.com>
2023-03-18 11:18:59 +0000

    #React-Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import CategoryMenu from './components/category-menu/category-menu';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+const Shop = () => {
+  return <h1> Im the shop page</h1>
+}
+
+
 const App = () => {
   return (
-    <CategoryMenu />
+    <Routes>
+      <Route path='/' element={ <Navigation />}>
+        <Route index element={ <Home />}></Route>
+        <Route path='shop' element={ <Shop />}></Route>
+      </Route>
+      
+    </Routes>
     )
 }
 
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from "react-router-dom" //Used to render the component before or after
+import './navigation.styles.scss';
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+const Navigation = () => {
+    return (
+        <Fragment>
+          <div className="navigation">
+            <Link className="logo-container" to='/'>
+                <CrwnLogo className='logo'></CrwnLogo>
+            </Link>
+            <div className="nav-links-container">
+                <Link className="nav-link" to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+          </div>
+          <Outlet></Outlet>
+        </Fragment>
+    )
+  }
+  
+  export default Navigation;
