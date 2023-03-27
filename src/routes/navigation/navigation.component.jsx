16c5a8c72571a4b66669380ef53c98c3ea55dadd
Everton Buzzi <evertonbuzzi@gmail.com>
2023-03-27 08:56:06 +0100

    # EB: Cart-Icon

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,9 @@ import './navigation.styles.scss';
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from '../../context/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
     
@@ -25,11 +28,10 @@ const Navigation = () => {
                             <Link className="nav-link" to='/auth'>
                                 SIGN IN
                             </Link>
-                        )
-                    
-                }
-                
+                        )}
+                    <CartIcon />
             </div>
+            <CartDropdown />
           </div>
           <Outlet></Outlet>
         </Fragment>
