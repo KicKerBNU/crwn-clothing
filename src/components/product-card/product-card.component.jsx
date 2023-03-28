a6fd278fe37ea3023de50eef4326076784484547
Everton Buzzi <evertonbuzzi@gmail.com>
2023-03-28 09:28:04 +0100

    EB: Add cart items

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,15 +1,22 @@
 import './product-card.styles.scss';
 import Button from '../button/button.component';
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
 
 const ProductCard = ({product}) => {
+    const { addItemToCart} = useContext(CartContext);
     const { name, price, imageUrl} = product;
+
+    const addProductToCart = () => addItemToCart(product);
+
+
     return (<div className='product-card-container'>
             <img alt={`${name}`} src={imageUrl}  />
             <div className='footer'>
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType={'inverted'}>Add to card</Button>
+            <Button buttonType={'inverted'} onClick={addProductToCart}>Add to card</Button>
     </div>)
 }
 
