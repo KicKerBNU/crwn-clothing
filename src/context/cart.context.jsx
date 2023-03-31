5840f19f9088ae6d26fa89bf53ec871b66e8920a
Everton Buzzi <evertonbuzzi@gmail.com>
2023-03-31 08:57:28 +0100

    EB: Checkout increment decrement

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -13,11 +13,24 @@ const addCartItem = (cartItems, productToAdd) => {
     return [...cartItems, { ...productToAdd, quantity: 1}];
 }
 
+const removeCartItem = (cartItems, productToRemove) => {
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToRemove.id)
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter(x => x.id !== productToRemove.id);
+    }else{
+        return cartItems.map((cartItem, index) => 
+            cartItem.id === productToRemove.id ? 
+            {...cartItem, quantity: cartItem.quantity -1} : 
+            cartItem)
+    }
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
+    removeItemToCart: () => {},
     countCartItems: 0,
 })
 
@@ -34,7 +47,12 @@ export const CartProvider  = ({children}) => {
     const addItemToCart = (productToAdd) => {
         setCartItems(addCartItem(cartItems, productToAdd));
     };
-    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, countCartItems};
+
+    const removeItemToCart = (productToRemove) => {
+        setCartItems(removeCartItem(cartItems, productToRemove));
+    }
+
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, removeItemToCart, cartItems, countCartItems};
     
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,19 +1,15 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from "react-router-dom" //Used to render the component before or after
-import './navigation.styles.scss';
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from '../../context/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { CartContext } from '../../context/cart.context';
-
+import './navigation.styles.scss';
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
     const { isCartOpen } = useContext(CartContext);
-
-    
-
         return (
         <Fragment>
           <div className="navigation">
