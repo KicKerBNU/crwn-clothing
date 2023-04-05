c3dbc35ea3d3bfea2ab985f45d5dd49e2fc2858a
Everton Buzzi <evertonbuzzi@gmail.com>
2023-04-05 09:22:37 +0100

    EB: Style Components

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,6 +1,6 @@
 
 import { useParams } from 'react-router-dom';
-import './category.styles.scss';
+import {CategoryContainer, CategoryTitle} from './category.styles.jsx';
 import { useContext, useState, useEffect} from 'react';
 import { CategoriesContext } from '../../context/categories.context';
 import ProductCard from '../../components/product-card/product-card.component';
@@ -19,12 +19,12 @@ const Category = () => {
 
     return (
         <Fragment>
-            <h2 className='category-title'>{category.toUpperCase()}</h2>
-            <div className='category-container'>
+            <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+            <CategoryContainer>
                 {products && products.map((product) => (
                     <ProductCard key={product.id} product={product} />
                 ))}
-            </div>    
+            </CategoryContainer>    
         </Fragment>
         
     )
