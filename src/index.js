5334b60354b9181cd041b5557363e531244185c7
Everton Buzzi <evertonbuzzi@gmail.com>
2023-04-23 09:34:29 +0100

    #EB: Redux useSelector

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'; 
-import { UserProvider } from './context/user.context';
 import { CategoriesProvider } from './context/categories.context';
 import { CartProvider } from './context/cart.context';
 import { store } from './store/store.js';
@@ -15,13 +14,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <UserProvider>
           <CategoriesProvider>
             <CartProvider>
               <App />
             </CartProvider>
           </CategoriesProvider>
-        </UserProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
---
+++ src/store/user/user.selector.js
@@ -0,0 +1,2 @@
+//This is just an abstraction of the state.user.currentUser to make it easier to use in other components
+export const selectCurrentUser = (state) => state.user.currentUser;
