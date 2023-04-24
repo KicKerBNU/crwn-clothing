6e8d551c9ed6c9c8171b9b884f995c2a55b480ee
Everton Buzzi <evertonbuzzi@gmail.com>
2023-04-24 07:28:40 +0100

    #EB: Using reselect to prevent re-render when is not necessary

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,19 @@
+import { createSelector } from 'reselect';
+
+const selectCategoryReducer = state => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  categoriesSlice => categoriesSlice.categories
+);
+
 //This is just an abstraction of the state.user.currentUser to make it easier to use in other components
-export const selectCategoriesMap = (state) => state.categories.categories
-    .reduce((acc, category) => {
+//This is a memoized selector preventing the component from re-rendering if the state hasn't changed
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => 
+    categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
-      return acc
-    }, {});
+      return acc;
+    }, {}));
