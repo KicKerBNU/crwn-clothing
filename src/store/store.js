737de60a83678d0e5472b6eb935d6731a2724586
Everton Buzzi <evertonbuzzi@gmail.com>
2023-04-26 09:23:42 +0100

    #EB: Reduce thunk

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import logger from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { rootReducer } from './root-reducer';
+import thunk from 'redux-thunk';
 
 const persistConfig = {
     key: 'root',
@@ -10,8 +11,20 @@ const persistConfig = {
     blacklist: ['user']
 };
 
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(persistedReducer, compose(applyMiddleware(logger)));
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+export const store = createStore(
+    persistedReducer, 
+    undefined,
+    composedEnhancers
+    );
 
 export const persistor = persistStore(store);
